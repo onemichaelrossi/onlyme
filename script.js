4900554d6426d09e84869086516ac6765aef9b05
Michael Rossi <michael@michaelrossi.org>
2026-07-21 20:42:14 +0000

    Add LinkedIn contact, remove GitHub, add portfolio descriptions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
 // Edit this value to update the LinkedIn link site-wide
-const LINKEDIN_URL = "https://www.linkedin.com/in/REPLACE_ME";
+const LINKEDIN_URL = "https://www.linkedin.com/in/michael-rossi-mbcs-citp-432b20a3";
 
 const linkedinEl = document.getElementById('linkedin-link');
 if (linkedinEl) linkedinEl.href = LINKEDIN_URL;
